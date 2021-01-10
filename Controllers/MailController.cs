01e9949324d99366b1a46c87dae84e91af9ba58e
MarikKV <kosaniakm@gmail.com>
2021-01-10 15:20:57 +0200

    add email send

---
+++ Controllers/MailController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CandleShop.Models;
+using System.Net.Mail;
+
+namespace CandleShop.Controllers
+{
+    public class MailController : ApiController
+    {
+        // GET: api/Mail
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Mail/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Mail
+        public string Post(gmail mail)
+        {
+            MailMessage mm = new MailMessage(mail.From, mail.To);
+            mm.Subject = mail.Subject;
+            mm.Body = mail.Body;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+
+            NetworkCredential nc = new NetworkCredential("candleshooplviv@gmail.com", "CandleShoopLviv838");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = nc;
+            smtp.Send(mm);
+
+            return "Mail send successfully!";
+        }
+
+    }
+}
---
+++ Models/GmailModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CandleShop.Models
+{
+    public class gmail
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
